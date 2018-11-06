752f15f111f46375877e32f0f7585dcbb3315cc7
Muhammad Khattab <msskzx@gmail.com>
2018-11-06 21:51:46 +0200

    initial commit
     - player movement
     - 3 types of sferes, randomly generated

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed;
+    private int region;
+    private int score;
+    public Text scoreText;
+
+    void Start()
+    {
+        speed = 10.0f;
+        region = 1;
+        score = 0;
+        scoreText.text = "Score: 0";
+    }
+
+    void Update()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0.0f, 0.0f));
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject gameObject = collision.gameObject;
+        if (gameObject.tag == "RedSfere" && region == 1 || gameObject.tag == "BlueSfere" && region == 2 || gameObject.tag == "GreenSfere" && region == 3)
+        {
+            Destroy(gameObject);
+            score++;
+        }
+        else
+        {
+            if (gameObject.tag != "Region")
+            {
+                GameOver();
+            }
+        }
+        
+ 
+    }
+
+    private void GameOver()
+    {
+    }
+}
---
+++ Assets/Scripts/RegionControlloer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+public class RegionControlloer : MonoBehaviour {
+
+    private float speed, spawnInterval, destroyAfter;
+    public GameObject[] regions;
+    public GameObject[] sferes;
+    private Vector3 spawnValues;
+    private int region = 0;
+
+    void Start () {
+        speed = 10.0f;
+        spawnInterval = 0.3f;
+        destroyAfter = 4.0f;
+        spawnValues = new Vector3(10, 0.5f, 30.0f);
+        InvokeRepeating("SpawnSfere", 2.0f, spawnInterval);
+	}
+
+    private void SpawnRegion()
+    {
+
+    }
+
+    private void SpawnSfere()
+    {
+        int sfereIndex = UnityEngine.Random.Range(0, sferes.Length);
+        Vector3 spawnPosition = new Vector3(UnityEngine.Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+        GameObject sfere = Instantiate(sferes[sfereIndex]) as GameObject;
+
+        sfere.transform.SetParent(transform);
+        sfere.transform.position = spawnPosition;
+        Destroy(sfere, destroyAfter);
+    }
+
+    void Update () {
+        
+	}
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.back  * speed * Time.deltaTime);
+    }
+}
