4ad6e0b2e4015bac50fa259e63792679ae707f40
Muhammad Khattab <msskzx@gmail.com>
2018-11-06 22:25:01 +0200

    update gitignore

--- Assets/Scripts/RegionControlloer.cs
+++ Assets/Scripts/RegionControlloer.cs
@@ -1,21 +1,39 @@
 ﻿using System;
 using UnityEngine;
 
-public class RegionControlloer : MonoBehaviour {
+public class RegionControlloer : MonoBehaviour
+{
 
     private float speed, spawnInterval, destroyAfter;
     public GameObject[] regions;
     public GameObject[] sferes;
     private Vector3 spawnValues;
-    private int region = 0;
+    private int region;
+    private int regionLength;
 
-    void Start () {
+    void Start()
+    {
+        region = 0;
+        regionLength = 100;
         speed = 10.0f;
         spawnInterval = 0.3f;
         destroyAfter = 4.0f;
         spawnValues = new Vector3(10, 0.5f, 30.0f);
         InvokeRepeating("SpawnSfere", 2.0f, spawnInterval);
-	}
+    }
+
+    void Update()
+    {
+        if (CheckRegionStart())
+        {
+            region = (region + 1) % regions.Length;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.back * speed * Time.deltaTime);
+    }
 
     private void SpawnRegion()
     {
@@ -31,14 +49,11 @@ public class RegionControlloer : MonoBehaviour {
         sfere.transform.SetParent(transform);
         sfere.transform.position = spawnPosition;
         Destroy(sfere, destroyAfter);
-    }
+    }   
 
-    void Update () {
-        
-	}
-
-    void FixedUpdate()
+    Boolean CheckRegionStart()
     {
-        transform.Translate(Vector3.back  * speed * Time.deltaTime);
+        return transform.position.z % regionLength == 0;
     }
+
 }
